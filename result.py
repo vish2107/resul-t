9b673ae421376b68482eaf35b8375f1a98ed72ce
vish2107 <95115245+vish2107@users.noreply.github.com>
2021-12-01 19:05:29 +0530

    Add files via upload

---
+++ result.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 18:10:30 2021
+
+@author: Vishal kale
+"""
+# rakesh result
+Marathi = 48
+english = 57
+math = 90
+science = 87
+social_sci = 48
+Marathi > 40
+english > 40
+math > 40
+science > 40
+social_sci > 40
+print(Marathi > 40,english > 40,math > 40,science > 40,social_sci > 40)
